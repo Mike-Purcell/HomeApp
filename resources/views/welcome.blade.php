78ab404bf36c89a76986095262ed544c8641eaba
Mike Purcell <mikep687@icloud.com>
2020-01-24 13:51:03 +0000

    Bulma WIP 24/1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,58 +3,64 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Home App</title>
-
-        <!-- Fonts -->
         <link rel="stylesheet" type="text/css" href="css/app.css">
-        <script src="https://kit.fontawesome.com/0d6ad6dcf2.js" crossorigin="anonymous"></script>
-        <!-- Styles -->
-            
+        <script src="https://kit.fontawesome.com/0d6ad6dcf2.js" crossorigin="anonymous"></script>            
     </head>
     <body>
         <!-- Header -->
         <nav class="navbar is-dark">
             <div class="navbar-brand">
-            <i class="fas fa-home fa-3x"></i>
+                <div class="navbar-item">
+                    <i class="fas fa-home fa-2x"></i>
+                </div>
                 <h1 class="title navbar-item">Home App</h1>
             </div>
         </nav>
         <!-- Main Body -->
         <div class="container">
             <div class="tile is-ancestor">
-                <!-- Calendar -->
-                <div class="tile is-parent box">
-                    <div class="tile is-child box">
-                        <h2>Calendar</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </p>
+                <div class="tile is-parent is-vertical box is-10">
+                    <!-- Calendar -->
+                    <div class="tile is-parent is-vertical box">
+                        <div>
+                            <h2 class="subtitle">Calendar</h2>
+                        </div>
+                        <div>
+                            Calendar API in here......
+                        </div>
                     </div>
-                
-                    <div id="weather">
-                        <h2>Weather</h2>
-                    </div>
-                    <div id="links">
-                        <h2>Links</h2>
-                        <a href="https://laravel.com/docs">Docs</a>
-                        <a href="https://laracasts.com">Laracasts</a>
-                        <a href="https://laravel-news.com">News</a>
-                        <a href="https://blog.laravel.com">Blog</a>
-                        <a href="https://nova.laravel.com">Nova</a>
-                        <a href="https://forge.laravel.com">Forge</a>
-                        <a href="https://vapor.laravel.com">Vapor</a>
-                        <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <div class="tile is-parent">
+                        <!-- Weather -->
+                        <div class="tile is-parent is-vertical box">
+                            <div>
+                                <h2 class="subtitle">Weather</h2>
+                            </div>
+                            <div>
+                                Weather API in here.......
+                            </div>
+                        </div>
+                        <!-- Links -->
+                        <div class="tile is-parent is-vertical box">
+                            <h2 class="subtitle">Links</h2>
+                            <div>
+                                <a href="https://laravel.com/docs">Docs</a>
+                                <a href="https://laracasts.com">Laracasts</a>
+                                <a href="https://laravel-news.com">News</a>
+                                <a href="https://blog.laravel.com">Blog</a>
+                                <a href="https://nova.laravel.com">Nova</a>
+                                <a href="https://forge.laravel.com">Forge</a>
+                                <a href="https://vapor.laravel.com">Vapor</a>
+                                <a href="https://github.com/laravel/laravel">GitHub</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="tile is-parent box">
-                    <h2>Shopping List</h2>
+                <!-- Shopping Lisst -->
+                <div class="tile is-parent is-vertical box">
+                    <h2 class="subtitle">Shopping List</h2>
                         @forelse ($item as $item)
-                            <div>
+                            <div class="is-lower-roman">
                                 {{ $item->id }}. {{ $item->description }}
                                 <form method="POST" action="/{{ $item->id }}">
                                     @csrf
@@ -68,24 +74,33 @@
                     
                     <form method="POST" action="/">
                         @csrf
-                            <div>    
-                                <label for="description"><h3>Description</h3></label>
-                                <input type="text" name="description">
+                        <div class="field">
+                            <label for="description" class="label is-small">Description</label>
+                            <div class="control has-icons-left has-icons-right">
+                              <input class="input is-small is-info is-rounded" type="text" name="description" placeholder="Description">
+                              <span class="icon is-small is-left">
+                                <i class="fas fa-shopping-cart"></i>
                             </div>
+                          </div>
                             <div>
                                 <button type="submit">Add Item</button>
                             </div>
                     </form>
                     <form method="POST" action="/contact">
                         @csrf
-                        <div>
-                            <label for="email"><h3>Email Address</h3></label>
-                            <input type="text" name="email">
+                        <div class="field">
+                            <label class="label is-small">Email Address</label>
+                            <div class="control has-icons-left">
+                              <input class="input is-small is-info is-rounded" type="email" placeholder="Email">
+                              <span class="icon is-small is-left">
+                                <i class="fas fa-envelope"></i>
+                              </span>
                             @error('email')
                             <div>{{ $message }}</div>
                             @enderror
+                            </div>
                         </div>
-                        @if(session('message'))
+                        <@if(session('message'))
                             <div>
                                 {{ session('message') }}
                             </div>
